125bda4c6f48a1561cb5127e19b7849781dc1094
astonaqulovsukhrob <astonaqulovsukhrob.email.com>
2021-05-10 20:08:46 +0300

    Growbe-Figma website files ready

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$('.apple').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3
+  });
+
+  $('.apple1').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
